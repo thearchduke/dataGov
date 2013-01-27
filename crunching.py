0fb9a3e6a9f62e8c8a9fb4268d88f101f0f76a30
thearchduke <tynanburke@gmail.com>
2013-01-27 14:31:04 -0800

    Basic skeleton complete

--- crunching.py
+++ crunching.py
@@ -1,20 +1,23 @@
+import random
+
 ####### Open the keyword and description files
 keyw = open('keywords.txt', 'r')
 descrip = open('descriptions.txt', 'r')
 
 
-####### Initialize the histogram arrays and the iterator
+####### Initialize the histogram arrays
 keyw_dict = {}
 descrip_dict = {}
-boring = ['a', 'an', 'the', 'and', 'of', 'to', 'is', 'in']
-#i = 0
+boring = ['a', 'an', 'the', 'and', 'of', 'to', 'is', 'in', 'as', 'on', 'for',\
+	'with', 'are', 'this', 'on', 'by', 'data', 'tri', 'http://www.epa.gov/tri/', "tri's",\
+	'it', 'which', 'from', 'at', 'http://www.epa.gov/tri/.', 'r', '(tri)']
 
 
 ####### Histogram basics:
 for line in keyw:
 	for word in line.split():
 		realWord = word.strip(',').lower()
-		if realWord in keyw_dict:
+		if realWord in keyw_dict and realWord not in boring:
 			keyw_dict[realWord] += 1
 		else:
 			keyw_dict[realWord] = 1
@@ -22,7 +25,7 @@ for line in keyw:
 for line in descrip:
 	for word in line.split():
 		realWord = word.strip(',').lower()
-		if realWord in keyw_dict:
+		if realWord in keyw_dict and realWord not in boring:
 			keyw_dict[realWord] += 1
 		else:
 			keyw_dict[realWord] = 1
@@ -36,13 +39,25 @@ top100 = []
 for key, value in sorted(keyw_dict.iteritems(), key=lambda (k,v): (v,k)):
 	top100.append((value, key))
 
-top100 = sorted(top100, reverse = True)[0:99]
+top100 = sorted(top100, reverse = True)[0:100]
 
-print top100
 
-#	if value > 10000000.0 / size:
-#		print key, value
 
+####### produce display html
+
+fout = open('cloud.html', 'w')
+fout.write('<!DOCTYPE html><html><head><title>Word cloud for data.gov</title></head><body><div style="width: 600px;">')
+
+divisor = float(top100[99][0])
+
+random.shuffle(top100)
+
+for pair in top100:
+	size = pair[0] / divisor * 5
+	fout.write("<span style='font-size: %spx;'>%s</span>  " % (size, pair[1]))
+
+fout.write('</div></body></html>')
+fout.close()
 
 
 keyw.close()
